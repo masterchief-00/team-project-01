4d2e8970cb558b424ddd952755a18701195f8e60
Jmukakalisa <90443432+Jmukakalisa@users.noreply.github.com>
2023-03-02 10:51:53 +0200

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,15 +3,12 @@ const {
   getAllUsers,
   createNewUser,
   findUser,
-  sendEmail,
   deleteUser,
 } = require("../controllers/userController");
 
 const router = express.Router();
 
 router.route("/").get(getAllUsers).post(createNewUser);
-
-router.route("/send-email").post(sendEmail);
 
 router.route("/:id").get(findUser).delete(deleteUser);
 
